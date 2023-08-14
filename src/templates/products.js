5a32bde0d981845e7fa864104d15e0546e5af5f1
GalicaYlber <galica.ylber@gmail.com>
2023-08-14 16:31:59 +0200

    Update Path

--- src/templates/products.js
+++ src/templates/products.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 
-import Nav from "../components/navbar/Nav.js";
-import ProudctList from "../components/productList/ProductList.js";
-import CartDrawer from "../components/cartDrawer/CartDrawer.js";
+import Nav from "/components/navbar/Nav.js";
+import ProudctList from "/components/productList/ProductList.js";
+import CartDrawer from "/components/cartDrawer/CartDrawer.js";
 
 import { CartProvider } from "../context/cart-context.js";
 
