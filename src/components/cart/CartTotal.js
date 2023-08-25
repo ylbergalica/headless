4e07c1f7dd653f69b01f0afaa1267092e3a6f149
GalicaYlber <galica.ylber@gmail.com>
2023-08-25 15:06:42 +0200

    Update CheckoutBtn
    -CheckoutBtn is now disabled when cart has no items

--- src/components/cart/CartTotal.js
+++ src/components/cart/CartTotal.js
@@ -10,7 +10,11 @@ const CartTotal = (props) => {
 			<h3 className={'w-[50%] text-xl lg:text-3xl tracking-[.1rem] mb-0' + (props.forDrawer ? ' lg:!text-2xl' : '')}>Total</h3>
 			<p className={'w-[50%] text-xl lg:text-3xl font-semibold mb-0' + (props.forDrawer ? ' lg:!text-2xl' : '')} >${cart.totalPrice?.amount}</p>
 			<div className={'w-full flex justify-center lg:mt-4'}>
-				<a href={cart.webUrl} target="_blank" className={'w-full lg:w-[90%] p-2 tracking-[.3rem] lg:tracking-[.4rem] text-base lg:text-xl text-white bg-nightsky-500 transition-all ease-out duration-500 hover:tracking-[.5rem] hover:bg-nightsky-550 hover:shadow-glow hover:shadow-nightsky-750'} >CHECK OUT</a>
+				<a href={cart.webUrl} target="_blank" className="w-full lg:w-[90%]">
+					<button className={'checkout-btn'} disabled={(cart.lineItems?.length > 0 ? false : true)} >
+						CHECK OUT
+					</button>
+				</a>
 			</div>
 		</div>
 	)
